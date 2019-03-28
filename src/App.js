3060c839af8a00b8eb1a11411e724b1eb8d5665c
tirempbs <tirem@fastmail.com>
2019-03-28 17:04:02 -0400

    mobile text color is still black

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Divider } from 'semantic-ui-react';
+import Nav from './Nav';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container className='topSlash' fluid >
+        <Nav />
+
+        <Container
+          textAlign='center'
+          className='topHeader'
+          content='Get a website in'
+        />
+        <Divider hidden />
+        <Container
+          textAlign='center'
+          className='topHeader'
+          content='15 minutes or less'
+        />
+
+      </Container>
     );
   }
 }
 
 export default App;
+
+
+
+
