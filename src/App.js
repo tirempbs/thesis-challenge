a99d4abc564e7b8e28666da6d4a2a9de6917d7e4
tirempbs <tirem@fastmail.com>
2019-03-28 17:14:53 -0400

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container, Divider } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import Nav from './Nav';
+import Content from './Content';
 
 class App extends Component {
   render() {
     return (
       <Container className='topSlash' fluid >
         <Nav />
-
-        <Container
-          textAlign='center'
-          className='topHeader'
-          content='Get a website in'
-        />
-        <Divider hidden />
-        <Container
-          textAlign='center'
-          className='topHeader'
-          content='15 minutes or less'
-        />
-
+        <Content />
       </Container>
     );
   }
