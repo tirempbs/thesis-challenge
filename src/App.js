d423b2217891650e4434b2296a7f6cc119e8d9a7
tirempbs <tirem@fastmail.com>
2019-04-01 11:27:20 -0400

    finished implementing 4 tiles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container } from 'semantic-ui-react';
+import { Container, Responsive } from 'semantic-ui-react';
 import Nav from './Nav';
 import Content from './Content';
+import Tiles from './Tiles';
+import CallToAction from './CallToAction';
 
 class App extends Component {
     render() {
         return (
-            <Container className='topSlash' fluid>
-                <Nav />
-                <Content />
-            </Container>
+            <>
+                <Responsive as={Container} minWidth={768}>
+                    <Container className='desktopTopSlash' fluid>
+                        <Nav />
+                        <Content />
+                    </Container>
+                    <Container className='desktopBottomSlash' fluid>
+                        <Tiles />
+                        <CallToAction />
+                    </Container>
+                </Responsive>
+                <Responsive as={Container} {...Responsive.onlyMobile}>
+                    <Container className='mobileTopSlash' fluid>
+                        <Nav />
+                        <Content />
+                    </Container>
+                    <Container className='mobileBottomSlash' fluid>
+                        <Tiles />
+                        <CallToAction />
+                    </Container>
+                </Responsive>
+            </>
         );
     }
 }
